15799e52903f2b8070188d4729a90fe2f595b36e
dh413 <148740246+dh413@users.noreply.github.com>
2024-05-28 14:50:15 +0900

    PR테스트

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -5,6 +5,12 @@ import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import { RecoilRoot } from "recoil";
 
+//이부분 궁금해요
+//recoil을 전역으로 쓰고싶어서 layout <RecoilRoot> 넣었는데요
+//<RecoilRoot>가 클라이언트 컴포넌트기 때문에 "use client"를 사용했어요
+//근데 Metadata이걸 사용하려면 서버컴포넌트여야지만 가능한 것으로 알고있는데
+//어떻게 처리하나요?
+
 // export const metadata: Metadata = {
 //   title: "땡큐",
 //   description: "description",
