16ff5ada235ef52970392b5773fd9388728e1ad8
dh413 <dh413@aladin.co.kr>
2024-05-29 21:34:21 +0900

    Map사용 & 주석제거

--- src/components/search/SearchNavigation.tsx
+++ src/components/search/SearchNavigation.tsx
@@ -5,99 +5,33 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function SearchNavigation() {
   const path = usePathname();
-  // const [searchedKeyword, setSearchedKeyword] =
-  //   useRecoilState(searchedKeywordState);
 
-  // const handleKeywordChange = (
-  //   event: React.ChangeEvent<HTMLInputElement>
-  // ): void => {
-  //   setSearchedKeyword(event.target.value);
-  // };
-
-  // const handleKeyDown = (
-  //   event: React.KeyboardEvent<HTMLInputElement>
-  // ): void => {
-  //   if (event.key === "Enter") {
-  //     handleSearch(event);
-  //   }
-  // };
+  const navItems = [
+    { href: "/search/customer", label: "고객" },
+    { href: "/search/item", label: "아이템" },
+    { href: "/search/wholesaler", label: "도매상" },
+    { href: "/search/publisher", label: "출판사" },
+    { href: "/search/distributor", label: "배본사" },
+    { href: "/search/order", label: "주문" },
+    { href: "/search/brand", label: "브랜드" },
+  ];
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <ul className="navbar-nav mx-auto">
-          <li className="nav-item">
-            <Link
-              href="/search/customer"
-              className={`nav-link ${
-                path === "/search/customer" ? "text-primary" : ""
-              }`}
-            >
-              고객
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              href="/search/item"
-              className={`nav-link ${
-                path === "/search/item" ? "text-primary" : ""
-              }`}
-            >
-              아이템
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              href="/search/wholesaler"
-              className={`nav-link ${
-                path === "/search/wholesaler" ? "text-primary" : ""
-              }`}
-            >
-              도매상
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              href="/search/publisher"
-              className={`nav-link ${
-                path === "/search/publisher" ? "text-primary" : ""
-              }`}
-            >
-              출판사
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              href="/search/distributor"
-              className={`nav-link ${
-                path === "/search/distributor" ? "text-primary" : ""
-              }`}
-            >
-              배본사
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              href="/search/order"
-              className={`nav-link ${
-                path === "/search/order" ? "text-primary" : ""
-              }`}
-            >
-              주문
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              href="/search/brand"
-              className={`nav-link ${
-                path === "/search/brand" ? "text-primary" : ""
-              }`}
-            >
-              브랜드
-            </Link>
-          </li>
+      <ul className="navbar-nav mx-auto">
+          {navItems.map((item) => (
+            <li className="nav-item" key={item.href}>
+            <Link href={item.href}
+                className={`nav-link ${path === item.href ? "text-primary" : ""}`}
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
+
     </nav>
   );
 }
