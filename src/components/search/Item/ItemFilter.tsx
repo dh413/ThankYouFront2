6606b4758e598791ec15256ffb583366a9beac97
dh413 <148740246+dh413@users.noreply.github.com>
2024-06-04 11:24:27 +0900

    Refactor: radio, checkbox 둘다 map으로 처리

--- src/components/search/Item/ItemFilter.tsx
+++ src/components/search/Item/ItemFilter.tsx
@@ -23,70 +23,67 @@ const Itemfilter: FC<ItemFilterProps> = ({
   changeFilter,
   changeCheckBoxFilter,
 }) => {
+  const radioConfigs = [
+    {
+      options: SEARCH_TYPE,
+      name: SEARCH.SEARCH_TYPE,
+      selectedValue: searchItemState.searchType,
+    },
+    {
+      options: MD_LEVEL,
+      name: SEARCH.MD_LEVEL,
+      selectedValue: searchItemState.mdLevel,
+    },
+    {
+      options: SORT_ORDER,
+      name: SEARCH.SORT_ORDER,
+      selectedValue: searchItemState.sortOrder,
+    },
+    {
+      options: BRANCH_TYPE,
+      name: SEARCH.BRANCH_TYPE,
+      selectedValue: searchItemState.branchType,
+    },
+  ];
+
+  const checkboxConfigs = [
+    {
+      label: "보유",
+      checked: searchItemState.includeOutOfStock,
+      name: SEARCH.INCLUDE_OUT_OF_STOCK,
+    },
+    {
+      label: "직배송",
+      checked: searchItemState.isDirectDelivery,
+      name: SEARCH.IS_DIRECT_DELIVERY,
+    },
+  ];
   return (
     <div className="row">
-      <div className="col-12 search-group">
-        <Radio
-          options={SEARCH_TYPE}
-          name={SEARCH.SEARCH_TYPE}
-          selectedValue={searchItemState.searchType}
-          onChange={(value) =>
-            changeFilter(SEARCH.SEARCH_TYPE as keyof SearchItemState, value)
-          }
-        />
-      </div>
-      <div className="col-12 search-group">
-        <Radio
-          options={MD_LEVEL}
-          name={SEARCH.MD_LEVEL}
-          selectedValue={searchItemState.mdLevel}
-          onChange={(value) =>
-            changeFilter(SEARCH.MD_LEVEL as keyof SearchItemState, value)
-          }
-        />
-      </div>
-      <div className="col-12 search-group">
-        <Radio
-          options={SORT_ORDER}
-          name={SEARCH.SORT_ORDER}
-          selectedValue={searchItemState.sortOrder}
-          onChange={(value) =>
-            changeFilter(SEARCH.SORT_ORDER as keyof SearchItemState, value)
-          }
-        />
-      </div>
-      <div className="col-12 search-group">
-        <Radio
-          options={BRANCH_TYPE}
-          name={SEARCH.BRANCH_TYPE}
-          selectedValue={searchItemState.branchType}
-          onChange={(value) =>
-            changeFilter(SEARCH.BRANCH_TYPE as keyof SearchItemState, value)
-          }
-        />
-      </div>
-      <div className="col-12 search-group">
-        <Checkbox
-          label="보유"
-          checked={searchItemState.includeOutOfStock}
-          onChange={() =>
-            changeCheckBoxFilter(
-              SEARCH.INCLUDE_OUT_OF_STOCK as keyof SearchItemState
-            )
-          }
-        />
-      </div>
-      <div className="col-12 search-group">
-        <Checkbox
-          label="직배송"
-          checked={searchItemState.isDirectDelivery}
-          onChange={() =>
-            changeCheckBoxFilter(
-              SEARCH.IS_DIRECT_DELIVERY as keyof SearchItemState
-            )
-          }
-        />
-      </div>
+      {radioConfigs.map((config) => (
+        <div className="col-12 search-group" key={config.name}>
+          <Radio
+            options={config.options}
+            name={config.name}
+            selectedValue={config.selectedValue}
+            onChange={(value) =>
+              changeFilter(config.name as keyof SearchItemState, value)
+            }
+          />
+        </div>
+      ))}
+
+      {checkboxConfigs.map((config) => (
+        <div className="col-12 search-group" key={config.name}>
+          <Checkbox
+            label={config.label}
+            checked={config.checked}
+            onChange={() =>
+              changeCheckBoxFilter(config.name as keyof SearchItemState)
+            }
+          />
+        </div>
+      ))}
     </div>
   );
 };
