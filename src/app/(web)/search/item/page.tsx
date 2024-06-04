28d487b3f476c94e1f1b64dc6e46bd30107bc3c6
dh413 <dh413@aladin.co.kr>
2024-06-04 21:05:28 +0900

    Refactor: boolean타입 네이밍 수정 - isResultView

--- src/app/(web)/search/item/page.tsx
+++ src/app/(web)/search/item/page.tsx
@@ -19,7 +19,7 @@ export default function SearchItemPage() {
     isDirectDelivery: true,
     sortOrder: 0,
   });
-  const [isResultViewState, setIsResultViewState] = useState<boolean>(false);
+  const [isResultView, setIsResultView] = useState<boolean>(false);
 
   const router = useRouter();
 
@@ -40,7 +40,7 @@ export default function SearchItemPage() {
         isDirectDelivery: searchItemState.isDirectDelivery.toString(),
         sortOrder: searchItemState.sortOrder.toString(),
       }).toString();
-      setIsResultViewState(true);
+      setIsResultView(true);
 
       router.push(`${SEARCH_ROUTES.URL.ITEM}?${query}`);
     }
@@ -72,7 +72,7 @@ export default function SearchItemPage() {
         changeFilter={changeFilter}
         changeCheckBoxFilter={changeCheckBoxFilter}
       />
-      {isResultViewState && <ItemResult />}
+      {isResultView && <ItemResult />}
       <ItemFooter />
     </>
   );
